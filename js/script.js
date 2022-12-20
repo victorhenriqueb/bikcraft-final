9d7bc73c7b4a918d4ecb114801cef05f5b540b8f
Victor Henrique <vhbatistafortunato2@gmail.com>
2022-12-20 15:49:08 -0300

    ativei o link das bicicletas e seguros na pagina de orçamento

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+// estilização do link do header
+
 const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
+function ativarLink(link) {
   const url = location.href;
   const href = link.href;
 
-  if(url.includes(href)){
-  link.classList.add("ativo");
+  if (url.includes(href)) {
+    link.classList.add("ativo");
   }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+// paramentros do url ( orçamento )
+
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(paramentro) {
+  const elemento = document.getElementById(paramentro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+paramentros.forEach(ativarProduto);
